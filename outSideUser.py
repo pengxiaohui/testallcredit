364cbc35598905224e0056a8922dfffdc335bb38
pengxiaohui <pengxiaohuidong@163.com>
2018-03-14 15:16:38 +0800

    add a py file test

---
+++ outSideUser.py
@@ -0,0 +1,50 @@
+# -*- coding:utf-8 -
+
+
+import tearchPhone.phone.apple.iphone6
+import tearchPhone.phone.apple.iphone6 as Iphone6
+
+# tearchPhone.phone.apple.iphone6.askPrice()
+# import sys
+# sys.path.append(r'E:\PycharmWorkSpace\testPhone\tearchPhone\phone\apple')
+
+
+import iphone6
+iphone6.askPrice()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
